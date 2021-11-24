72b91953c2bf13547a3f98ce8a0ae2e5ac5b05b9
NareeyaChenu <nachenu@gmail.com>
2021-11-24 13:26:22 +0700

    add contact

--- src/components/contents/contact/Contact.js
+++ src/components/contents/contact/Contact.js
@@ -1,10 +1,18 @@
 import React from 'react'
 
+
 function Contact() {
     return (
         <div>
-            Contact
+           
+           <ui>
+           <li><a href = "https://web.facebook.com/WinonaCosmetic/?_rdc=1&_rdr">วิโนน่า คอสเมติกส์</a></li>
+           <li> <a href = "https://www.instagram.com/winonaofficial_th/">winonaofficial_th</a></li>
+           <li> <a href = "https://www.winonafeminine.com/en">Winona Feminine (official) </a></li>
+           </ui>
+            
         </div>
+        
     )
 }
 
