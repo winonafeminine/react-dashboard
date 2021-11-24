8b2d1ccfc30f04e2067122b1e16e77cb162b19f5
Has12Wan34 <rbae1998@gmail.com>
2021-11-24 13:22:59 +0700

    add wan

--- src/components/contents/contact/Contact.js
+++ src/components/contents/contact/Contact.js
@@ -2,10 +2,17 @@ import React from 'react'
 
 function Contact() {
     return (
-        <div>
-            Contact
+        <div className="Contact">
+        <header className="App-header">
+            <h1 className="App">winonafeminine</h1>
+            <h1 className=".bb">About</h1>
+            <h2>Contact Form</h2> 
+            <p>Name*</p>
+            <p>Email*</p>
+            <p>Message*</p>
+      </header>
         </div>
     )
 }
 
-export default Contact
+export default Contact;
