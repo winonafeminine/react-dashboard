b171e154cdd0e532ff1628b23c37d048ed6bfa58
winonafeminine <winonafeminine.dev2021@gmail.com>
2021-11-24 16:56:53 +0700

    first use of mui

--- src/components/contents/contact/Contact.js
+++ src/components/contents/contact/Contact.js
@@ -1,17 +1,47 @@
 import React from 'react'
+import Box from '@mui/material/Box';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import Divider from '@mui/material/Divider';
+import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
+import LocalShippingIcon from '@mui/icons-material/LocalShipping';
 
-
-function Contact() {
-    return (
-        <div>
-           <ui>
-           <li><a href = "https://web.facebook.com/WinonaCosmetic/?_rdc=1&_rdr">วิโนน่า คอสเมติกส์</a></li>
-           <li> <a href = "https://www.instagram.com/winonaofficial_th/">winonaofficial_th</a></li>
-           <li> <a href = "https://www.winonafeminine.com/en">Winona Feminine (official) </a></li>
-           </ui>
-        </div>
-        
-    )
+export default function Contact() {
+  return (
+    <Box sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
+      <nav aria-label="main mailbox folders">
+        <List>
+          <ListItem disablePadding>
+            <ListItemButton>
+            <ListItemIcon>
+                <AddShoppingCartIcon/>
+              </ListItemIcon>
+              <ListItemText primary="วิโนน่า คอสเมติกส์" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                <LocalShippingIcon />
+              </ListItemIcon>
+              <ListItemText primary="winonaofficial_th" />
+            </ListItemButton>
+          </ListItem>
+        </List>
+      </nav>
+      <Divider />
+      <nav aria-label="secondary mailbox folders">
+        <List>
+          <ListItem disablePadding>
+            <ListItemButton>
+              <ListItemText primary="Winona Feminine (official) " />
+            </ListItemButton>
+          </ListItem>
+        </List>
+      </nav>
+    </Box>
+  );
 }
-
-export default Contact;
