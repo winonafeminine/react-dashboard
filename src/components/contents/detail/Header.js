d740df8958865046e5b071388be5ff60182bba27
suha3001 <suhabaka01@gmail.com>
2021-11-24 22:19:08 +0700

    add detail

---
+++ src/components/contents/detail/Header.js
@@ -0,0 +1,18 @@
+import React from "react"
+import './Detail.css'
+
+
+function Header ()
+{
+    return (
+        <div>
+            <header>
+                <h2>Emotional Agility</h2>
+            </header>
+        </div>
+
+
+)
+}
+
+export default Header
