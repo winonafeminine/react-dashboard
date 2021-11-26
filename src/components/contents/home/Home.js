04e29c8506fd253d841852126b040a80f994192e
winonafeminine <winonafeminine.dev2021@gmail.com>
2021-11-26 07:59:28 +0700

    edit home layout

--- src/components/contents/home/Home.js
+++ src/components/contents/home/Home.js
@@ -1,19 +1,39 @@
 import React from 'react';
-import './style/Home.css';
+// import './style/Home.css';
 import Homeheader from './Homeheader';
 import Homecontainer from './Homecontainer';
+import Box from '@mui/material/Box';
 
 
 function Home() {
     return (  
-        <div className="home">
-            <div className="home header">
-                <Homeheader />
-            </div>         
-            <div className="home container">
-                <Homecontainer />
-            </div>
-        </div> 
+        <React.Fragment>
+            {/* container */}
+            <Box sx={{
+                display: 'flex',
+                justifyContent: 'center'
+            }}>
+                <Box className="home" sx={{
+                    '&.MuiBox-root': theme => ({
+                        display: 'flex',
+                        flexDirection: 'column',
+                        alignItems: 'center',
+                        width: '65%'
+                    })
+                }}>
+                    <Box className="home header" sx={{
+                        
+                    }}>
+                        <Homeheader />
+                    </Box>         
+                    <Box className="home container" sx={{
+                        width: '100%'
+                    }}>
+                        <Homecontainer />
+                    </Box>
+                </Box> 
+            </Box>
+            </React.Fragment>
     )
 }
 
