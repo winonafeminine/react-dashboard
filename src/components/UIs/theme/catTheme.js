1b1eb1bb03016dbfa6f1aad33226bc4dc9bea2b1
winonafeminine <winonafeminine.dev2021@gmail.com>
2021-11-27 10:28:33 +0700

    add promt font in cat

---
+++ src/components/UIs/theme/catTheme.js
@@ -0,0 +1,11 @@
+import createTheme from '@mui/material/styles/createTheme';
+
+
+export const catTheme = createTheme({
+    typography: {
+        fontFamily: [
+            '"Prompt"', 
+            'sans-serif'
+        ].join(',')
+    }
+});
