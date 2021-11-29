724e2389b270a9c2259c34797056a00622cf068c
winonafeminine <winonafeminine.dev2021@gmail.com>
2021-11-29 08:40:58 +0700

    fix duplecate

--- src/App.js
+++ src/App.js
@@ -11,13 +11,10 @@ import Contact from './components/contents/contact/Contact';
 import About from './components/contents/about/About';
 import Products from './components/contents/product/Products';
 import ProductDetail from './components/contents/product/ProductDetail';
-<<<<<<< HEAD
 import Kdetail from './components/contents/contact/kdetail';
 
-=======
 import Dtdetail from './components/contents/detail/Dtdetail';
 import ProductDetail1 from './components/contents/about/ProductDetail1';
->>>>>>> fd21acd60a482155ae12ed35e26f3b67f1cc4d43
 function App() {
 
   return (
@@ -26,15 +23,9 @@ function App() {
         <ul className="navbar-group">
           <li className="navbar"><Link to="/" className="btn">Home</Link></li>
           <li className="navbar"><Link to="/product" className="btn">Catsitory</Link></li>
-<<<<<<< HEAD
-          <li className="navbar"><Link to="/detail" className="btn">Detail</Link></li>
           <li className="navbar"><Link to="/contact" className="btn">KPOP Center</Link></li>
-          <li className="navbar"><Link to="/about" className="btn">About</Link></li>
-=======
           <li className="navbar"><Link to="/detail" className="btn">Sweater</Link></li>
-          <li className="navbar"><Link to="/contact" className="btn">Contact</Link></li>
           <li className="navbar"><Link to="/about" className="btn">Clothes</Link></li>
->>>>>>> fd21acd60a482155ae12ed35e26f3b67f1cc4d43
         </ul>
         <Routes>
           <Route exact path="/" element={<Home/>} />
@@ -43,12 +34,9 @@ function App() {
           <Route exact path="/contact" element={<Contact/>} />
           <Route exact path="/about" element={<About/>} />
           <Route exact path="/cats/detail" element={<ProductDetail/>} />
-<<<<<<< HEAD
           <Route exact path="/kcenter/detail" element={<Kdetail/>} />
-=======
           <Route exact path="/suha/sweater" element={<Dtdetail/>} />
           <Route exact path="/clot/detail" element={<ProductDetail1/>} />
->>>>>>> fd21acd60a482155ae12ed35e26f3b67f1cc4d43
         </Routes>
       </Router>
     </div>
